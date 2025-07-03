45def3a33eabfd66d5e706c67359e78c6fd5e2e0
0031400 <liuxiongzhuo@163.com>
2025-07-03 15:26:46 +0800

    init

---
+++ lua/plugins/config.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+  },
+  {
+    "neovim/nvim-lspconfig",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "python",
+        "typescript",
+        "yaml",
+        "go",
+        "css",
+      },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "json-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "lua-language-server",
+      },
+    },
+  },
+}
